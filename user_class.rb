24cab9b3a105333b82b4d25afb95a8d7f6ef3dfb
Devin Helmgren <devincollege@gmail.com>
2019-02-25 10:00:27 -0800

    methods added

--- user_class.rb
+++ user_class.rb
@@ -1,1 +1,20 @@
-ruby
+class User
+  def username
+    puts "Dee Del Rosario"
+  end
+
+  def email
+    return "ada@adadev.org"
+  end
+
+  def summary
+    return "#{name}: #{email}"
+  end
+end
+
+
+dee = User.new()
+kaida = User.new()
+
+dee.username()
+kaida.username()

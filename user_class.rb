b1dceebf7e2c651d87ea8402f15570be29943584
Devin Helmgren <devincollege@gmail.com>
2019-02-25 12:13:25 -0800

    finished off lec 2

--- user_class.rb
+++ user_class.rb
@@ -1,69 +1,89 @@
 class User
+  attr_reader :address, :name #, :email
 
-  def initialize name: "default", email: "default@default.pizza"
+  attr_writer :pets#, :email
+
+  attr_accessor :email
+
+  #def address
+  # return @address
+  #end
+
+  def initialize(name: "default", email: "default@default.pizza")
     @name = name
     @email = email
     @super_secret = "Yr super cool"
+    @address = "home"
     puts summary
   end
 
-
-  def name
-    return @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  # def you_can_call_me_dee
-  #   @name = "Dee"
+  # def name
+  #   return @name
   # end
 
-  def email
-    return @email
-  end
+  # # def you_can_call_me_dee
+  # #   @name = "Dee"
+  # # end
 
-  def email=(new_email)
-    @email = new_email
-  end
+  # def email
+  #   return @email
+  # end
+
+  # def email=(new_email)
+  #   @email = new_email
+  # end
 
   def summary
-    return "#{name}: #{email} Also #{@super_secret}"
+    return "#{name}: #{email} pets: #{@pets}"
   end
 end
 
 class Cat
-  def name 
-    returns @name
+  attr_accessor :name
+  attr_reader :fur_type
+
+  def initialize name: "Meowtimer", fur_type: "black"
+    @name = name
+    @fur_type = fur_type
+  end
+
+  def lounge
+    puts "#{self.name} is lounging"
+  end
+
+  def play some_other_cat
+    puts "#{self.name} is playing with #{some_other_cat.name}"
   end
 end
+
+calico = Cat.new(name: "Luna", fur_type: "Calico")
+calico.lounge
+
+tortie = Cat.new(name: "Tonks", fur_type: "Tortoiseshell")
+calico.play(tortie)
+tortie.play(calico)
+
 
 # def add_idk (value)
 #   newthing = value
 # end
 
-dee = User.new(name: "Amy", email: "amysemail@amy.net")
+# dee = User.new(name: "Amy", email: "amysemail@amy.net")
+# puts "#{dee.address}"
+# dee.pets = "Samson"
+# puts "#{dee.summary}"
+
+# dee.address = "home"
+
 # dee.name = "Dee"
 # dee.email = "dee@adadev.org"
-puts dee.name
+# puts dee.name
 
-dee = User.new()
-
-
-
-
-
-
-
-
+# dee = User.new()
 
 # dee.you_can_call_me_dee
 
-
-
 # add_idk(value)
-
 
 # kaida = User.new()
 # kaida.name = "Kaida"
